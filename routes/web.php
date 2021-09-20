018e63acc3f959e6b5bc9bec31aaf0641c7f78f8
Hector Saldivia <canal25.hector@gmail.com>
2021-09-20 18:09:36 -0300

    Logs anadidos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('A');
+    return view('trabajo1');
 });
 
 Route::get('/index', function () {
